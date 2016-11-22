4549716f285d5e40a551a68fa656b7ecd37e8edf
Steve McGee <steve.mcgee@vivintsolar.com>
2016-11-22 16:24:11 -0700

    adds some cool problems to study

--- my_own_js/_Blank_or_changingCode_forPractice.js
+++ my_own_js/_Blank_or_changingCode_forPractice.js
@@ -1,38 +1,71 @@
-// Given a list of integers and a single sum value, return the first two values 
-// (parse from the left please) in order of appearance that add up to form the sum.
-// EXAMPLES:
-// sum_pairs([11, 3, 7, 5],         10)
-// #              ^--^      3 + 7 = 10
-// == [3, 7]
+// Given a number, return a string with dash'-'marks before and after each odd integer, but do not begin or end the string with a dash mark.
 
-// sum_pairs([4, 3, 2, 3, 4],         6)
-// #          ^-----^         4 + 2 = 6, indices: 0, 2 *
-// #             ^-----^      3 + 3 = 6, indices: 1, 3
-// #                ^-----^   2 + 4 = 6, indices: 2, 4
-// #  * entire pair is earlier, and therefore is the correct answer
-// == [4, 2]
+// Examples:
 
-// sum_pairs([0, 0, -2, 3], 2)
-// #  there are no pairs of values that can be added to produce 2.
-// == None/nil/undefined (Based on the language)
+// dashatize(274) -> '2-7-4'
+// dashatize(6815) -> '68-1-5'
 
-// sum_pairs([10, 5, 2, 3, 7, 5],         10)
-// #              ^-----------^   5 + 5 = 10, indices: 1, 5
-// #                    ^--^      3 + 7 = 10, indices: 3, 4 *
-// #  * entire pair is earlier, and therefore is the correct answer
-// == [3, 7]
-
+const num = 274;
 
 // Solution #1 - mine
-var sum_pairs = function(ints, s){
-  var result = [];
-  for (var i = 0; i < ints.length; i++) {
-    for (var j = 1; j < ints.length; j++) {
-      if (ints[i] + ints[i + j] === s) {
-        result.push(ints[i]);
-        result.push(ints[i + j]);
-        return result;
+const dashatize = (num) => {
+  if (isNaN(num)) {
+      return 'NaN';
+  }
+  num = Math.abs(num);
+  let arr = num.toString().split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.length === 1) {                         // if the num is only a single digit simply return it
+      return arr.toString();
+    }
+    if (i !== 0) {                                  // if it isn't the first one
+      if (arr[i] % 2 !== 0) {                         // and if the num is odd then -num
+        arr[i] = '-' + arr[i];
+      }                                    
+      if (arr[i] % 2 === 0 && arr[i-1] % 2 !== 0) {   // and if the num is even AND the one before it was odd then -num
+        arr[i] = '-' + arr[i];
       }
     }
   }
+  return arr.join('');
+};
+
+
+// Solution #2 - mine refactored a little
+const dashatize = (num) => {
+  if (isNaN(num)) {
+      return 'NaN';
+  }
+  num = Math.abs(num);
+  let arr = num.toString().split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.length === 1) {                         // if the num is only a single digit simply return it
+      return arr.toString();
+    }
+    if (i !== 0) {                                  // if it isn't the first one
+      if (arr[i] % 2 !== 0) {                         // and if the num is odd then -num
+        arr[i] = '-' + arr[i];
+      }                                    
+      if (arr[i] % 2 === 0 && arr[i-1] % 2 !== 0) {   // and if the num is even AND the one before it was odd then -num
+        arr[i] = '-' + arr[i];
+      }
+    }
+  }
+  return arr.join('');
+};
+
+
+
+// Solution #3 - using regex but seems quite readable - and after understanding it I love it!
+function dashatize(num) {
+  return String(num)
+    .replace(/([13579])/g, "-$1-")    // if its an odd number add dash before and after
+    .replace(/--+/g, "-")             // if there are double dashes then replace with a single dash
+    .replace(/(^-|-$)/g, "")          // if the string starts or ends with a dash remove it
+}
+
+
+// Solution #4 - there will always be a dash between numbers unless there is more than one even number
+function dashatize(num) {
+  return isNaN(num) ? 'NaN' : num.toString().match(/([13579]|[02468]+)/g).join('-');
 };
