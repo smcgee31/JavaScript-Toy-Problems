a94df09d7d9dada36f5302ae5c2c63c058a09990
smcgee31 <solodesign31@gmail.com>
2017-03-08 01:05:15 -0700

    Add more toys

--- my_own_js/ceasarsCipher_rot13.js
+++ my_own_js/ceasarsCipher_rot13.js
@@ -6,6 +6,8 @@
 
 // Write a function which takes a ROT13 encoded string as input and returns a decoded string.
 
+// NOTE: THIS SOLUTION SET IS ONLY FOR UPPERCASE LETTERS - DOES NOT WORK FOR LOWER CASE LETTERS
+
 
 // Solution #1 - mine
 function rot13(str) { 
@@ -28,4 +30,24 @@ rot13("SERR PBQR PNZC");
 // Solution #2 - using ES6, regEx, and some cool math
 function rot13(str) { 
   return str.replace(/[A-Z]/g, (L) => String.fromCharCode(65 + (L.charCodeAt(0) - 65 + 13) % 26));
+}
+
+
+// Solution #3 - mine - SOLVES FOR UPPER, LOWER, AND OTHER CHARACTERS
+// like "10 + 2 is twelve"  >>  "10 + 2 vf gjryir"
+function rot13(str) { 
+  var arr = str.split('');
+  var turned = [];
+  arr.map(function (val) {
+    if ( val.charCodeAt(0) > 77 && val.charCodeAt(0) < 91 ) {
+      turned.push( String.fromCharCode(val.charCodeAt(0) - 13) );
+    } else if ( val.charCodeAt(0) < 78 && val.charCodeAt(0) > 64 ) {
+      turned.push( String.fromCharCode(val.charCodeAt(0) + 13) );
+    } else if ( val.charCodeAt(0) > 109 && val.charCodeAt(0) < 123 ) {
+      turned.push( String.fromCharCode(val.charCodeAt(0) - 13) );
+    } else if ( val.charCodeAt(0) < 110 && val.charCodeAt(0) > 96 ) {
+      turned.push( String.fromCharCode(val.charCodeAt(0) + 13) );
+    } else { turned.push(val); }
+  });
+  return turned.join('');
 }
